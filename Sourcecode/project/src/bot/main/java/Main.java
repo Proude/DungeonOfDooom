2b29491525dc2b4f937a211963896ebe208f735a
mattsi <mattsi@jansky.co.uk>
2016-12-11 17:05:25 +0000

    updated diary

--- Sourcecode/project/src/bot/main/java/Main.java
+++ Sourcecode/project/src/bot/main/java/Main.java
@@ -10,7 +10,14 @@ public class Main {
      * @param args expects 1 argument of ID for match to join
      */
     public static void main(String args[]) {
-        UUID matchId = UUID.fromString(args[0]);
+        UUID matchId = null;
+        try {
+            matchId = UUID.fromString(args[0]);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
         Bot bot = new Bot();
         bot.play(matchId);
